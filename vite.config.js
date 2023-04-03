c99190217596991d7ea3a1b759afdd05c7af22eb
tyler36 <tyler36@users.noreply.github.com>
2023-04-04 08:50:56 +0900

    📦 `tailwindCSS` (#3)

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            protocol: 'wss',
+            host: `${process.env.DDEV_HOSTNAME}`,
+        },
+    },
 });
